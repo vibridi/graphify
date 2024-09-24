fd550aa2dec35cd1a403b7143ec76a2b60d39bdf
Gabriele <gabriele@nulab.com>
2024-09-24 14:49:57 +0200

    Fix centering of graph within SVG canvas

--- internal/svg/svg.go
+++ internal/svg/svg.go
@@ -20,7 +20,7 @@ func Draw(g graph.Layout, w io.Writer, opts Options) {
 	}
 	padding := opts.CanvasPadding
 
-	canvas.Start(int(right-left+float64(padding)*2), int(bottom-top+float64(padding)))
+	canvas.Start(int(right-left+float64(padding)*2), int(bottom-top+float64(padding)*2))
 	if opts.ShowTimestamp {
 		canvas.Text(10, 10+padding, time.Now().String(), "font-size:20px;fill:black")
 	}
@@ -31,13 +31,13 @@ func Draw(g graph.Layout, w io.Writer, opts Options) {
 	canvas.DefEnd()
 
 	for _, n := range g.Nodes {
-		canvas.Rect(int(n.X)+padding, int(n.Y)+padding*2, int(n.W), int(n.H), "fill:white;stroke:black")
+		canvas.Rect(int(n.X)+padding, int(n.Y)+padding, int(n.W), int(n.H), "fill:white;stroke:black")
 
 		text := n.ID
 		if opts.PrintNodePosition {
 			// text += "-" + strconv.Itoa(n.LayerPos)
 		}
-		canvas.Text(int(n.X)+int(n.W)/2+padding, int(n.Y)+int(n.H)/2+padding*2, text, "text-anchor:middle;font-size:30px;fill:black")
+		canvas.Text(int(n.X)+int(n.W)/2+padding, int(n.Y)+int(n.H)/2+padding, text, "text-anchor:middle;font-size:30px;fill:black")
 	}
 
 	for _, e := range g.Edges {
@@ -58,7 +58,7 @@ func regularEdges(canvas *svgo.SVG, e graph.Edge, opts Options) {
 	var xs, ys []int
 	for _, p := range e.Points {
 		xs = append(xs, int(p[0])+opts.CanvasPadding)
-		ys = append(ys, int(p[1])+opts.CanvasPadding*2)
+		ys = append(ys, int(p[1])+opts.CanvasPadding)
 	}
 
 	marker := "marker-end"
